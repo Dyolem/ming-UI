b07812b4175f87f11663eb0132a9b2be3941aacf
Adam Davis <dyolem9@gmail.com>
2024-04-06 22:10:28 +0800

    feat(ControlPanel):add tooltip demo

---
+++ packages/ming-UI/src/control-panel/demos/tooltip.vue
@@ -0,0 +1,57 @@
+<script setup>
+import { ref } from 'vue'
+
+const initSliderPosition = ref({
+  traveledDistance: 50,
+  verticalToTraveledDistance: 0,
+})
+function formatter(axis) {
+  console.log(axis)
+  const traveledDistance = `this ${axis}`
+  return traveledDistance
+}
+const backgroundStyle = {
+  width: '200px',
+  height: '200px',
+  backgroundColor: '#e9eaff',
+  borderRadius: '10px',
+}
+
+// const distance = ref({
+//   traveledDistance: 44,
+//   verticalToTraveledDistance: 9,
+// })
+const dimensionalDistance = ref({
+  traveledDistance: 66,
+  verticalToTraveledDistance: 25,
+})
+function dimensionalFormatter(axis, vertical) {
+  console.log(axis, vertical)
+  const traveledDistance = `this ${axis}`
+  const verticalToTraveledDistance = `this ${vertical}`
+  return {
+    traveledDistance,
+    verticalToTraveledDistance,
+  }
+}
+</script>
+
+<template>
+  <div class="container">
+    <MControlPanel :model-value="initSliderPosition" :formatter-tooltip="formatter" />
+    <div>
+      <MControlPanel v-model="dimensionalDistance" :background-style="backgroundStyle" :dimensional-movement="true" :formatter-tooltip="dimensionalFormatter" />
+      <div class="position">
+        X:<span>{{ dimensionalDistance.traveledDistance }}</span>
+        Y:<span>{{ dimensionalDistance.verticalToTraveledDistance }}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+    display: flex;
+    justify-content: space-around;
+}
+</style>
