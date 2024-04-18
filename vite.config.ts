db1b8f35035ded1dec839205a45063c9720df12a
Adam Davis <dyolem9@gmail.com>
2024-04-18 10:53:13 +0800

    chore(MING-UI):Delete the configuration associated with vite-plugin-svg-icons

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
-import path from 'node:path'
-import process from 'node:process'
 import { defineConfig } from 'vite'
 import { vitepressDemo } from 'vite-plugin-vitepress-demo'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import tsxResolveTypes from 'vite-plugin-tsx-resolve-types'
-import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 import Component from 'unplugin-vue-components/vite'
 import alias from './alias'
 import { mingUIResolver } from './scripts/ming-ui-resolver'
@@ -22,24 +19,6 @@ export default defineConfig({
     // Component({
     //   resolvers: [mingUIResolver()],
     // }),
-    createSvgIconsPlugin({
-      // Specify the icon folder to be cached
-      iconDirs: [path.resolve(process.cwd(), 'packages/icons/src/svg')],
-      // Specify symbolId format
-      symbolId: 'icon-[dir]-[name]',
-
-      /**
-       * custom insert position
-       * @default: body-last
-       */
-      inject: 'body-last',
-
-      /**
-       * custom dom id
-       * @default: __svg__icons__dom__
-       */
-      customDomId: '__svg__icons__dom__',
-    }),
   ],
   resolve: {
     alias,
