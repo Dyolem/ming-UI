6fc166f871ec29814734327900d8a9de323e7fa7
Adam Davis <dyolem9@gmail.com>
2024-04-25 23:59:15 +0800

    feat(Rate):Complete the initial effect of the rating component

--- packages/ming-UI/src/rate/Rate.vue
+++ packages/ming-UI/src/rate/Rate.vue
@@ -1,33 +1,90 @@
-<script setup lang="ts">
+<script setup>
+import { computed, ref } from 'vue'
+import { nanoid } from 'nanoid'
+
 defineOptions({
   name: 'MRate',
 })
+const props = defineProps({
+  rating: {
+    type: Number,
+    default: 0,
+  },
+  maxRating: {
+    type: Number,
+    default: 5,
+  },
+  iconComponent: {
+    type: Object,
+    // default: () => 'svg', // 默认使用svg标签，使用者可以传入任何组件
+  },
+})
+console.log(props.rating, props.maxRating)
+const clipId = ref(nanoid())
+// const clipWidth = computed(() => `${props.rating / props.maxRating * 100}%`)
+const clipWidth = '8'
 </script>
 
 <template>
-  <div class="container">
-    <svg width="100%" height="100%" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg">
-      <path
-        d="M23.9986 5L17.8856 17.4776L4 19.4911L14.0589 29.3251L11.6544 43L23.9986 36.4192L36.3454 43L33.9586 29.3251L44 19.4911L30.1913 17.4776L23.9986 5Z"
-        fill="url(#half-gradient)" stroke="#333" stroke-width="3" stroke-linejoin="round"
-      />
+  <div class="rate">
+    <svg width="0" height="0">
       <defs>
-        <linearGradient id="half-gradient" x1="0%" y1="0%" x2="100%" y2="0%">
-          <stop offset="70%" stop-color="gold" />
-          <stop offset="30%" stop-color="transparent" />
-        </linearGradient>
+        <clipPath :id="clipId">
+          <rect :width="clipWidth" height="16" />
+        </clipPath>
       </defs>
     </svg>
+    <svg width="0" height="0">
+      <defs>
+        <filter :id="`${clipId}-filter`">
+          <feGaussianBlur stdDeviation="5" />
+        </filter>
+      </defs>
+    </svg>
+    <div class="svg-container">
+      <component :is="iconComponent" class="init-svg fulled-stroke lower-svg" />
+      <component :is="iconComponent" class="init-svg fulled-stroke clip-color upper-svg" :style="{ 'clip-path': `url(#${clipId})` }" />
+    </div>
+    <!-- <div
+      v-for="index in Math.ceil(maxRating)"
+      :key="index"
+      class="star-container"
+    >
+      <div class="star-clip">
+        <component :is="iconComponent" :style="{ 'clip-path': `url(#${clipId})` }" />
+      </div>
+      <component :is="iconComponent" class="star-background" />
+    </div> -->
   </div>
 </template>
 
-<style scoped>
-.container {
-    width: 20px;
-    height: 20px;
-    /* background-color: pink; */
-    /* mask-image: url(./star.svg); */
-    /* mask-repeat: no-repeat; */
-    /* mask-size: 100% 100%; */
+<style>
+.init-svg {
+
+  position: absolute;
+  width: 16px;
+  height: 16px;
+  fill: gray;
+}
+.star-container {
+  display: inline-block;
+  position: relative;
+}
+.svg-container {
+  position: relative;
+}
+
+.clip-color {
+  fill: gold;
+}
+
+.fulled-stroke {
+  stroke: #333;
+}
+.lower-svg {
+    /* 未选中的图标使用灰度 */
+    fill: aquamarine;
+    filter: grayscale(50%);
+
 }
 </style>
