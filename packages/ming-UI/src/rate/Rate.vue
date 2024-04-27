797c0e31647fc4cf9fd1248a4c9c5c369ab51c12
Adam Davis <dyolem9@gmail.com>
2024-04-27 23:57:46 +0800

    feat(rate):Data binding of the rate component is complete

--- packages/ming-UI/src/rate/Rate.vue
+++ packages/ming-UI/src/rate/Rate.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
-import { computed, onMounted, ref } from 'vue'
-import { nanoid } from 'nanoid'
+import { computed, nextTick, onMounted, ref, watch, watchEffect } from 'vue'
+
+// import { nanoid } from 'nanoid'
 import type { rateProps } from './interface.ts'
 import like from './like.vue'
 
@@ -24,94 +25,202 @@ const props = withDefaults(defineProps<rateProps>(), {
   max: 10,
   rateIconCount: 5,
 })
+const emit = defineEmits(['update:modelValue'])
 
-const svgContainerRef = ref<HTMLDivElement | null>(null)
+type svgContainerType = HTMLDivElement | null
+const svgContainerRef = ref<svgContainerType[]>([])
 const iconRef = ref<InstanceType<typeof props.iconComponent>>(null)
-const clipId = ref(nanoid())
+// const clipId = ref(nanoid())
 // const clipWidth = computed(() => `${props.rating / props.maxRating * 100}%`)
-const clipWidth = ref<number>(0)
+
 const isFull = ref<boolean>(false)
 const strokeColor = computed(() => {
   if (isFull.value)
     return props.fillColor
   else return props.stroke
 })
-onMounted(() => {
-  initSvgContainer()
-  generateScoreArr(props.modelValue)
-})
-function initSvgContainer() {
-  if (svgContainerRef.value === null)
-    return
 
-  svgContainerRef.value.style.width = `${props.size}px`
-  svgContainerRef.value.style.height = `${props.size}px`
+interface clipWidthArrType {
+  id: number
+  clipWidth: number
+  score: number
 }
-function currentPosition(e: MouseEvent) {
-  if (svgContainerRef.value === null)
-    return
-  const svgContainerRect = svgContainerRef.value?.getBoundingClientRect()
-  if (props.allowHalf) {
-    const halfBoundary = props.size / 2
-    console.log(e.offsetX, halfBoundary)
-
-    if (e.offsetX <= halfBoundary) {
-      clipWidth.value = halfBoundary
-      isFull.value = false
-    }
-    else {
-      clipWidth.value = props.size
-      isFull.value = (props.stroke !== 'none')
-    }
-  }
-  else {
-    clipWidth.value = e.offsetX
-
-    if (e.offsetX >= svgContainerRect.width - 2 && props.stroke !== 'none')
-      isFull.value = true
-    else isFull.value = false
-  }
-}
+const clipWidthArr = ref<Array<clipWidthArrType>>([])
 const maximumScore = computed(() => {
   return props.max / props.rateIconCount
 })
-function generateScoreArr(scoreValue: number) {
-  console.log(scoreValue)
 
-  const scoreArr = []
+onMounted(async () => {
+  await nextTick()
+  console.log(svgContainerRef.value.length)
+
+  initSvgContainer()
+  clipWidthArr.value = generateClipWidthArr(props.modelValue)
+  watch(() => props.modelValue, (newVal) => {
+    clipWidthArr.value = generateClipWidthArr(newVal)
+  })
+})
+function initSvgContainer() {
+  // if (!svgContainerRef.value.length)
+  //   return
+  console.log(svgContainerRef.value, svgContainerRef.value.length)
+
+  for (const svgContainer of (svgContainerRef.value)) {
+    console.log(svgContainer)
+    if (!svgContainer)
+      return
+    svgContainer.style.width = `${props.size}px`
+    console.log(svgContainer.style.width)
+
+    svgContainer.style.height = `${props.size}px`
+  }
+}
+
+function updateClipWidthArr(index: number) {
+  const newArr = clipWidthArr.value.map((item, i) => {
+    if (i < index)
+      return { ...item, score: maximumScore.value }
+    else if (i > index)
+      return { ...item, score: 0 }
+    else
+      return item
+  })
+
+  clipWidthArr.value = newArr
+}
+
+function mark(index: number, e: MouseEvent) {
+  if (!svgContainerRef.value.length)
+    return
+  if (svgContainerRef.value[index] === null)
+    return
+  const svgContainerRect = svgContainerRef.value[index]!.getBoundingClientRect()
+  let score = 0
+  if (props.allowHalf) {
+    const halfBoundary = props.size / 2
+
+    if (e.offsetX < halfBoundary) {
+      // clipWidth.value = halfBoundary
+      score = maximumScore.value / 2
+      console.log(score)
+
+      clipWidthArr.value[index].score = score
+      // isFull.value = false
+    }
+    else {
+      // clipWidth.value = props.size
+      score = maximumScore.value
+      clipWidthArr.value[index].score = score
+
+      // isFull.value = (props.stroke !== 'none')
+    }
+  }
+  else {
+    // clipWidth.value = e.offsetX
+    score = e.offsetX / svgContainerRect.width * maximumScore.value
+    console.log(e.offsetX, svgContainerRect.width)
+
+    clipWidthArr.value[index].score = score
+    // if (e.offsetX >= svgContainerRect.width - 2 && props.stroke !== 'none')
+    //   isFull.value = true
+    // else isFull.value = false
+  }
+  updateClipWidthArr(index)
+
+  emit('update:modelValue', getAllScore())
+}
+
+function getAllScore() {
+  let totalScore = 0
+  for (const item of clipWidthArr.value)
+    totalScore += item.score
+  return totalScore
+}
+// function clipWidthConvertToScore(widthArr: clipWidthArrType[]) {
+//   let totalWidth = 0
+//   for (const item of widthArr)
+//     totalWidth += item.clipWidth
+// }
+function scoreConvertToClipWidth(score: number) {
+  let clipWidth = 0
+  if (props.allowHalf) {
+    const scoreBoundary = maximumScore.value / 2
+    if (score > scoreBoundary) {
+      isFull.value = true
+      clipWidth = props.size
+    }
+
+    else if (score > 0 && score <= scoreBoundary) {
+      isFull.value = false
+      clipWidth = props.size / 2
+    }
+
+    else {
+      isFull.value = false
+      clipWidth = 0
+    }
+  }
+  else {
+    clipWidth = score / maximumScore.value * props.size
+  }
+  return clipWidth
+}
+
+const scoreObj = ref<clipWidthArrType>({
+  id: 0,
+  clipWidth: 0,
+  score: 0,
+})
+function generateClipWidthArr(scoreValue: number) {
+  let residualScore = scoreValue
+  const clipWidthArr = []
+
   for (let i = 0; i < props.rateIconCount; i++) {
     let score = 0
-    if (scoreValue - maximumScore.value * i > 0)
+
+    if (residualScore >= maximumScore.value)
       score = maximumScore.value
-    else score = scoreValue - maximumScore.value * i
-    const scoreObj = {
+    else
+      score = residualScore
+
+    residualScore = residualScore - score
+
+    scoreObj.value = {
       id: i,
+      clipWidth: scoreConvertToClipWidth(score),
       score,
     }
-    scoreArr.push(scoreObj)
-    console.log(scoreObj)
+
+    clipWidthArr.push(scoreObj.value)
   }
-  return scoreArr
+  return clipWidthArr
 }
 </script>
 
 <template>
   <div class="rate">
-    <svg width="0" height="0">
-      <defs>
-        <clipPath :id="clipId">
-          <rect :width="clipWidth" :height="size" />
-        </clipPath>
-      </defs>
-    </svg>
-    <div class="svg-container" @click="currentPosition">
+    <span>{{ modelValue }}</span>
+    <div v-for="(item, index) in clipWidthArr" ref="svgContainerRef" :key="item.id" class="svg-container" @click="mark(index, $event)">
+      <svg width="0" height="0">
+        <defs>
+          <clipPath :id="`${item.id}`">
+            <rect :width="item.clipWidth" :height="size" />
+          </clipPath>
+        </defs>
+      </svg>
       <component :is="iconComponent" ref="iconRef" class="init-svg lower-svg" :class="stroke !== 'none' ? 'stroke' : ''" />
-      <component :is="iconComponent" class="init-svg  clip-color upper-svg" :class="stroke !== 'none' ? 'stroke' : ''" :style="{ 'clip-path': `url(#${clipId})` }" />
+      <component :is="iconComponent" class="init-svg  clip-color upper-svg" :class="stroke !== 'none' ? 'stroke' : ''" :style="{ 'clip-path': `url(#${item.id})` }" />
     </div>
   </div>
 </template>
 
 <style>
+.rate {
+  padding: 0 5px;
+  width: 200px;
+  display: flex;
+  justify-content: space-between;
+}
 .init-svg {
   stroke-width: v-bind(strokeWidth);
   position: absolute;
