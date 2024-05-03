2bccfc442f91d79606cabfd04946e6af7220953c
Adam Davis <dyolem9@gmail.com>
2024-05-03 23:37:08 +0800

    feat(Rate):update basic demo

--- packages/ming-UI/src/rate/demos/basic.vue
+++ packages/ming-UI/src/rate/demos/basic.vue
@@ -1,10 +1,16 @@
 <script setup>
 import { ref } from 'vue'
+import { BookMarkOne, Like, Star } from '../../../../icons/src/index.ts'
 
-const score = ref(5)
+const icon = [Star, Like, BookMarkOne]
+const score = ref(2)
 </script>
 
 <template>
   <span>{{ score }}</span>
-  <MRate v-model="score" />
+  <MRate v-model="score" :clearable="true" :allow-half="true" :rate-icon-count="4" :fill-color="['#99A9BF', '#F7BA2A']" :low-threshold="2" :high-threshold="3" :icon-component="icon">
+    <template #default="{ templateScore }">
+      {{ templateScore + 1 }}
+    </template>
+  </MRate>
 </template>
