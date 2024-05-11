6e0fa8037e84a3c4517fa626e62fe932f18ff77a
Adam Davis <dyolem9@gmail.com>
2024-05-12 00:32:50 +0800

    feat(notification): Add SVG icons for different notification types

---
+++ packages/ming-UI/src/notification/components/Error.vue
@@ -0,0 +1,3 @@
+<template>
+  <svg width="24" height="24" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M6 11L11 6L24 19L37 6L42 11L29 24L42 37L37 42L24 29L11 42L6 37L19 24L6 11Z" fill="#f56c6c" stroke="#f56c6c" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" /></svg>
+</template>
