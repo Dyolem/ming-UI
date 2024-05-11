d7a8068e89152516173af35ae6261b203be39e07
Adam Davis <dyolem9@gmail.com>
2024-05-12 01:02:58 +0800

    feat(notification): Update demos

--- packages/ming-UI/src/notification/demos/basic.vue
+++ packages/ming-UI/src/notification/demos/basic.vue
@@ -1,25 +1,10 @@
-<docs>
----
-title: 基本使用
----
-
-  这是notification的基本使用
-</docs>
-
 <script setup lang="ts">
 import { MNotification } from 'ming-UI'
-import { getCurrentInstance, h } from 'vue'
 
-// import Test from './components/test.vue'
-
-const instance = getCurrentInstance()
 function info() {
-  MNotification.info({
-    title: '通知',
-    content: 'hello world',
-    appContext: instance?.appContext,
-    duration: 1000,
-    showClose: true,
+  MNotification.notify({
+    content: 'This an info message.',
+    duration: 3000,
   })
 }
 </script>
@@ -27,7 +12,7 @@ function info() {
 <template>
   <div>
     <m-button @click="info">
-      弹框
+      notify
     </m-button>
   </div>
 </template>
