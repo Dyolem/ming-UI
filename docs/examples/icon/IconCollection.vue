0e50c52750e130d84edbe318e93f4e94bb0987df
Adam Davis <dyolem9@gmail.com>
2024-06-04 11:09:12 +0800

    docs(icon):Update the demo component displayed in the icon documentation

--- docs/examples/icon/IconCollection.vue
+++ docs/examples/icon/IconCollection.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { useClipBoard } from '@ming-UI/utils'
-import { DislikeTwo, Like, MIcon } from '@ming-UI/icons'
-import { ref } from 'vue'
+import { BookMark, BookMarkOne, ColorExtractor, DislikeTwo, Download, Like, MIcon, Mute, Pause, PlayOne, Star, VoiceOne, VolumeDown, VolumeUp } from '@ming-UI/icons'
+import { markRaw, ref } from 'vue'
 
 function copyIconsName(event) {
   if (event.target?.textContent === undefined)
@@ -10,50 +10,117 @@ function copyIconsName(event) {
   useClipBoard(content)
 }
 const basicIcons = ref(new Map([
-  ['Like', Like],
-  ['DislikeTwo', DislikeTwo],
+  ['Like', markRaw(Like)],
+  ['DislikeTwo', markRaw(DislikeTwo)],
+]))
+const mediaIcons = ref(new Map([
+  ['PlayOne', markRaw(PlayOne)],
+  ['Pause', markRaw(Pause)],
+  ['Mute', markRaw(Mute)],
+  ['volumeUp', markRaw(VolumeUp)],
+  ['VoiceOne', markRaw(VoiceOne)],
+  ['voiceDown', markRaw(VolumeDown)],
+]))
+const officeIcons = ref(new Map([
+  ['Download', markRaw(Download)],
+  ['BookMark', markRaw(BookMark)],
+  ['BookMarkOne', markRaw(BookMarkOne)],
+]))
+const editIcons = ref(new Map([
+  ['ColorExtractor', markRaw(ColorExtractor)],
+  ['Star', markRaw(Star)],
+
 ]))
 </script>
 
 <template>
-  <ul :class="$style.grid" class="vp-raw" @click="copyIconsName($event)">
-    <li v-for="([name, basic], index) in basicIcons" :key="index">
-      <MIcon>
-        <component :is="basic" />
-      </MIcon>
-      <span>{{ name }}</span>
-    </li>
-  </ul>
+  <div :class="$style.container" @click="copyIconsName($event)">
+    <div :class="$style.collectionItem">
+      <h3>基础</h3>
+      <ul :class="$style.grid" class="vp-raw">
+        <li v-for="([name, comp], index) in basicIcons" :key="index">
+          <MIcon>
+            <component :is="comp" />
+          </MIcon>
+          <span>{{ name }}</span>
+        </li>
+      </ul>
+    </div>
+    <div :class="$style.collectionItem">
+      <h3>多媒体音乐</h3>
+      <ul :class="$style.grid" class="vp-raw">
+        <li v-for="([name, comp], index) in mediaIcons" :key="index">
+          <MIcon>
+            <component :is="comp" />
+          </MIcon>
+          <span>{{ name }}</span>
+        </li>
+      </ul>
+    </div>
+    <div :class="$style.collectionItem">
+      <h3>办公文档</h3>
+      <ul :class="$style.grid" class="vp-raw">
+        <li v-for="([name, comp], index) in officeIcons" :key="index">
+          <MIcon>
+            <component :is="comp" />
+          </MIcon>
+          <span>{{ name }}</span>
+        </li>
+      </ul>
+    </div>
+    <div :class="$style.collectionItem">
+      <h3>编辑</h3>
+      <ul :class="$style.grid" class="vp-raw">
+        <li v-for="([name, comp], index) in editIcons" :key="index">
+          <MIcon>
+            <component :is="comp" />
+          </MIcon>
+          <span>{{ name }}</span>
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <style module>
-  .grid {
-          width: 100%;
-          display: grid;
-          display: grid;
-          grid-template-columns: repeat(7, 1fr);
-          border-left:1px solid #dccfeb;
-          border-top:1px solid #dccfeb;
-          cursor:pointer;
-
+.contianer {
+  display: flex;
+  justify-content: space-around;
+  width: 100%;
 }
-  .grid li {
-          border-right:1px solid #dccfeb;
-          border-bottom:1px solid #dccfeb;
-          aspect-ratio: 1 / 0.8; /* 保持宽高比*/
-          display: flex;
-          flex-direction:column;
-          justify-content: center;
-          align-items: center;
-          font-size:12px;
-        }
-  .grid li:hover {
-            background-color: #f2f6fc;
-            transition: all 0.3s linear;
-        }
-  .box {
-      display: flex;
-      justify-content: space-around;
-      width: 100%;
-    }
+
+.grid {
+  margin-top: 20px;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(80px, 1fr));
+  cursor: pointer;
+  border-bottom:1px solid #dccfeb;
+}
+
+.grid li {
+  aspect-ratio: 1 / 0.8;
+  /* 保持宽高比*/
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-size: 12px;
+  border-left: 1px solid #dccfeb;
+  border-top: 1px solid #dccfeb;
+  border-bottom:1px solid #dccfeb;
+}
+.grid li:last-child {
+  border-right: 1px solid #dccfeb;
+}
+
+.grid li:hover {
+  background-color: #f2f6fc;
+  transition: all 0.3s linear;
+}
+
+.box {
+  display: flex;
+  justify-content: space-around;
+  width: 100%;
+}
 </style>
